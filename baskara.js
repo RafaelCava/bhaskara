b43442db3999231c3a98c4e6fdea382126be29c9
Rafael Cavalcante <ra.facavalcante@hotmail.com>
2021-06-11 09:59:48 -0300

    up1

--- baskara.js
+++ baskara.js
@@ -1,14 +1,15 @@
-function bhaskara(A,B,C){
-    console.log(`O problema é: A:${A} B:${B} C:${C}`)
+var bhaskara = (A,B,C) => {
+    console.log(`O problema é: A:${A} B:${B} C:${C}`);
     
     const resultDelta = Math.pow(B, 2) - 4 * A * C;
     console.log(`Resultado de Delta é ${resultDelta}`);
-
+    
     const bhaskara1 = (-B + Math.sqrt(resultDelta)) / (2 * A);
     console.log(`Logo X linha 1 é igual ${bhaskara1}`)  
     
     const bhaskara2 = (-B - Math.sqrt(resultDelta)) / (2 * A);
     console.log(`Logo X linha 2 é igual ${bhaskara2}`)  
 }
+
 
 bhaskara(2,5,2);
